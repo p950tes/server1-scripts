58af33a49de8c7c2af5bc4d1acaff5f362811b7b
user <user@server1>
2018-10-04 22:54:45 +0200

    added new script to simplify fail2ban administration, and updated newExternalUser

---
+++ jail-status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function clean {
+	sed 's/^[^A-Z]*//g' | sed -r 's/\s+/ /g' | sed 's/:\s*/:/g'
+}
+
+jails=$(fail2ban-client status | clean | grep "Jail list" | cut -d':' -f2 | sed 's/,//g')
+
+echo "Jails: $jails"
+
+for jail in $jails; do
+	echo -e "\033[0;31m$jail\033[0m"
+	status=$(fail2ban-client status $jail | clean)
+	num_banned=$(echo "$status" | grep "Currently banned" | cut -d':' -f2)
+	echo " * Banned: $num_banned"
+	if [ $num_banned -gt 0 ]; then
+		banned=$(echo "$status" | grep "Banned IP list" | cut -d':' -f2-)
+		echo " * IP(s): $banned"
+	fi
+	echo 
+done
+
+echo "View status of a jail: fail2ban-client status {jail}"
+echo "To unban someone: fail2ban-client set {jail} unbanip {ip}"
+
--- newExternalUser.sh
+++ newExternalUser.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-# Set the script to exit on error
-set -e
+function ensureRoot {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Error: root permissions are required" 1>&2
+		exit 1
+	fi
+}
+function addUser {
+	addcmd="useradd -g sftp-external -d /srv/sftp -s /usr/sbin/nologin $USER"
+	echo -e "Creating user\n$addcmd"
+	$addcmd
+}
+function addUserToGroups {
+	echo -e "\nAdding user to groups"
+	groups="media software www-external sftp-external"
+	
+	for group in $groups; do
+		cmd="usermod -a -G $group $USER"
+		echo "$cmd"
+		$cmd
+	done
+}
+function setUserPassword {
+	echo -e "\nSetting user password"
+	numtries=0
+	while ! passwd "$USER"; do
+		echo "Setting password failed, try again."
+		numtries=$((numtries + 1))
+		if [ $numtries -gt 3 ]; then 
+		   echo "Giving up, please set password for user $USER manually using passwd"
+		   exit 1
+	   fi
+	done
+}
 
-user=${1}
+ensureRoot
 
-while [ -z "$user" ]; do
+USER=${1}
+
+while [ -z "$USER" ]; do
 	printf "Enter username: "
-	read user
+	read USER
 done
 
-groups="media software www-external sftp-external"
+addUser
+addUserToGroups
 
-addcmd="useradd -g sftp-external -d /srv/sftp -s /usr/sbin/nologin $user"
-echo -e "Creating user\n$addcmd"
-$addcmd
-
-echo -e "\nAdding user to groups"
-
-for group in $groups; do
-	cmd="usermod -a -G $group $user"
-	echo "$cmd"
-	$cmd
-done
-
-echo -e "\nSetting user password"
-
-while ! passwd "$user"; do
-	echo "Setting password failed, try again."
-done
+setUserPassword
 
